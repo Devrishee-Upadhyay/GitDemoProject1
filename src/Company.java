bc0bbef0aeaa7d1765035d911f7708e1789cc6c9
Dev <devrishee.upadhyay14@gmail.com>
2020-06-09 16:25:26 +0530

    Changing contents of the Company class

--- src/Company.java
+++ src/Company.java
@@ -2,11 +2,7 @@
 public class Company {
 
 	public static void main(String[] args) {
-		System.out.println("Best fuckin comoany in the world");
-		
-		System.out .println("What makes you say that?");
-		
-		System.out.println("I makes me say it");
+		System.out.println("I'm Trying to learn new things here");
 	}
 
 }
